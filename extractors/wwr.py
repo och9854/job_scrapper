bf204b6f7b749f36f83e85e95ea50b3512d6d1e5
och9854 <och9854@naver.com>
2023-09-14 13:34:26 +0900

    extract_indeed func

---
+++ extractors/wwr.py
@@ -0,0 +1,36 @@
+from requests import get
+from bs4 import BeautifulSoup
+
+
+def extract_wwr_jobs(keyword) -> list:
+    base_url = 'https://weworkremotely.com/remote-jobs/search?term='
+    search_term = ''  # any language or serach terms
+    response = get(f'{base_url}{search_term}')
+
+    if response.status_code != 200:
+        print("Can't request webiste")
+    else:
+        result = []
+        soup = BeautifulSoup(response.text, "html.parser")
+        jobs = soup.find_all("section", class_='jobs')
+
+        for job_section in jobs:
+            job_posts = job_section.find_all("li")
+            job_posts.pop(-1)  # remove last item: 'view-all' li
+
+            for post in job_posts:
+                anchor = post.find_all('a')[1]  # get only second one
+                link = anchor['href']
+                # print(link)
+                company, kind, region = anchor.find_all(
+                    "span", class_='company')
+                title = anchor.find("span", class_='title')
+                job_data = {
+                    'link': f"https://weworkremotely.com/{link}",
+                    'company': company.string,
+                    'region': region.string,
+                    'position': title.string
+                }
+                result.append(job_data)
+
+        return result
---
+++ google.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+driver = webdriver.Chrome(
+    '/Users/ohchanghyun/Desktop/nomad_coder/web_scrapper/chromedriver')
+url = 'https://www.google.com'
+driver.get(url)
+
+#
